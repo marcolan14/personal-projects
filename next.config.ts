83d38b8cf5e3d6e86bfdce1acbcc66fdd874d8ed
marcolan85 <marco.lanza@qomodo.me>
2026-07-07 15:23:46 +0200

    Update allowedDevOrigins to include additional IP address (Qomodo office)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.1.53'],
+  allowedDevOrigins: ['192.168.1.53', '10.0.20.22'],
   /* config options here */
 };
 
